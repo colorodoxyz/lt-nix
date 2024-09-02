d604192400486bbc60da2c2ef761a89ea7c03200
Spencer Liu <spencer.liu.liu@gmail.com>
2024-09-02 13:06:47 -0700

    update flake

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -11,6 +11,7 @@
     file.".ssh/allowed_signers".text = ''
     spencer.liu.liu@gmail.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFYOC0HTedqrgULQ7WOhqwQ7OCapQk+q4ZLOE4knjSkW
     '';
+    sessionPath = [ "~/.cargo/bin" ];
   };
 
   services = {
